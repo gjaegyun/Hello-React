f4145c38dddc2ae8fddd39fbc808400f37ae208d
gjaegyun <jaegyungim06@gmail.com>
2023-07-17 21:07:42 +0900

    test commit

--- src/Dice.js
+++ src/Dice.js
@@ -2,7 +2,10 @@ import diceBlue01 from './assets/dice-blue-1.svg';
 
 function Dice(props) {
     console.log(props);
-    return <img src={diceBlue01} alt="주사위" />;
+    return
+    <>
+     <img src={diceBlue01} alt="주사위" /><img src={diceBlue01} alt="주사위" />;
+    </>
 }
 
 export default Dice;
