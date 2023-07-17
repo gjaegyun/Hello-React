1e628476eb456f8ebccaa512e23692a91af36613
gjaegyun <jaegyungim06@gmail.com>
2023-07-17 23:51:47 +0900

    commit Dice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {useState} from 'react';
 import Dice from './Dice';
+import Button from './button';
 
 function App() {
+    const [num, setNum] = useState(1);
+    
+    const handleRollClick = () => {
+        setNum(3);
+    };
+
     return (
     <div>
-        <Dice color="blue:"/>
+        <div>
+            <Button onClick={handleRollClick}>던지기</Button>
+            <Button>처음부터</Button>
+        </div>
+        <Dice color = "red" num = {num}/>
     </div>
     );
 }
