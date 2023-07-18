7d32f5a831cfd6b47eb76bfc5fce793dd90d3b40
gjaegyun <jaegyungim06@gmail.com>
2023-07-18 10:13:19 +0900

    commit AddFunction

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import {useState} from 'react';
 import Dice from './Dice';
 import Button from './button';
 
+function random(n) {
+    return Math.ceil(Math.random() * n);
+}
 function App() {
     const [num, setNum] = useState(1);
     
     const handleRollClick = () => {
-        setNum(3);
+        const nextNum = random(6);
+        setNum(nextNum);
     };
 
+    const handleClearClick = () => {
+        setNum(1);
+    }
+
     return (
-    <div>
         <div>
-            <Button onClick={handleRollClick}>던지기</Button>
-            <Button>처음부터</Button>
+            <div>
+                <Button onClick={handleRollClick}>던지기</Button>
+                <Button onClick={handleClearClick}>처음부터</Button>
+            </div>
+            <Dice color ='red' num={num} />
         </div>
-        <Dice color = "red" num = {num}/>
-    </div>
     );
 }
 
